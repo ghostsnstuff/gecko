f4c98d6c92b1bc17fd82c79afa767d306a244c8b
jared <jaredhalpert90@gmail.com>
2014-03-26 01:27:22 -0700

    mods

--- gecko.js
+++ gecko.js
@@ -1,21 +1,20 @@
-// converts string to tc binary
-// converts string to hex
-
 exports.gecko = gecko
 
 function gecko() {
 	if(!(this instanceof gecko)) return new gecko()
+	// converts string to tc binary
 	this.toBinary = function(str) {
 		var binary = {}
 		for (var i = 0; i < str.length ; i++) {
-		 binary[str[i]] = zeroFill(str.charCodeAt(i).toString(2), 8)
+		 binary[str[i]] = zeroFill(str.charCodeAt(i).toString(2), 8) // radix 2
 		}	
 		return binary
 	}
+	// converts string to hex
 	this.toHex = function(str) {
 		var hex = {}
 		for (var i = 0; i < str.length ; i++) {
-		  hex[str[i]] = str.charCodeAt(i).toString(16)
+		  hex[str[i]] = str.charCodeAt(i).toString(16) // radix 16
 		}	
 		return hex
 	}
@@ -26,6 +25,5 @@ function zeroFill(number, width) {
   if (width > 0) {
     return new Array(width + (/\./.test(number) ? 2 : 1)).join('0') + number
   }
-  return number + '' // always return a string
+  return number + '' 
 }
-
