ce2750b19ef6ad8aaffb370e457d41341166e1fd
ghostsnstuff <jaredhalpert90@gmail.com>
2014-03-26 01:23:08 -0700

    Update gecko.js

--- gecko.js
+++ gecko.js
@@ -4,21 +4,21 @@
 exports.gecko = gecko
 
 function gecko() {
-	if(!(this instanceof gecko)) return new gecko()
-	this.toBinary = function(str) {
-		var binary = {}
-		for (var i = 0; i < str.length ; i++) {
-		 binary[str[i]] = zeroFill(str.charCodeAt(i).toString(2), 8)
-		}	
-		return binary
-	}
-	this.toHex = function(str) {
-		var hex = {}
-		for (var i = 0; i < str.length ; i++) {
-		  hex[str[i]] = str.charCodeAt(i).toString(16)
-		}	
-		return hex
-	}
+  if(!(this instanceof gecko)) return new gecko()
+    this.toBinary = function(str) {
+      var binary = {}
+      for (var i = 0; i < str.length ; i++) {
+        binary[str[i]] = zeroFill(str.charCodeAt(i).toString(2), 8) // radix 2
+      }	
+      return binary
+    }
+    this.toHex = function(str) {
+      var hex = {}
+      for (var i = 0; i < str.length ; i++) {
+        hex[str[i]] = str.charCodeAt(i).toString(16) // radix 16
+      }	
+      return hex
+    }
 }
 
 function zeroFill(number, width) {
@@ -26,6 +26,6 @@ function zeroFill(number, width) {
   if (width > 0) {
     return new Array(width + (/\./.test(number) ? 2 : 1)).join('0') + number
   }
-  return number + '' // always return a string
+  return number + ''
 }
 
