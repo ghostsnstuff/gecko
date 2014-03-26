1e03fd9418d232f7b69883ce0ad1d6646a2e276a
ghostsnstuff <jaredhalpert90@gmail.com>
2014-03-26 02:26:32 -0700

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,11 +1,6 @@
 var gecko = require('../gecko').gecko()
-<<<<<<< HEAD
-	, assert = require('assert')
-	, str = 'nodejs'
-=======
   , assert = require('assert')
   , str = 'nodejs'
->>>>>>> f951bb72c3d0e669cb4b8591eafa4041cc04287e
 	
 var binary = gecko.toBinary(str)
 var actual = JSON.stringify(binary)
@@ -17,8 +12,4 @@ var actual = JSON.stringify(hex)
 var expected = JSON.stringify({n:'6e',o:'6f',d:'64',e:'65',j:'6a',s:'73'})
 assert.equal(actual, expected, 'gecko.toHex(str) failed')
 
-<<<<<<< HEAD
 console.log('yay everything passed')
-=======
-console.log('yay everything passed')
->>>>>>> f951bb72c3d0e669cb4b8591eafa4041cc04287e
